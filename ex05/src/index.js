0e0a6ac5a49a25ba3c816b5756ffb80e70942c6e
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-09 21:37:27 +0200

    Added ex05/day12

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,7 +1,21 @@
 // Create a monitorsListArray array here
-
-// End of monitorsListArray array
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+// End of monitorsListArray array                      
 // Create a function below this line
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+    var brojac = 0;
+    var monitorsList = [];
+    for (i = 0; i < monitorsListArray.length ; i++) {
+        brojac++;
+        monitorsList.push(monitorsListArray[i]);
+        monitorsList.push(brojac);
+        newMonitorsList.push(monitorsList);
+    }                         
+     return monitorsList;
 
+}
+console.log(myMonitorsFunction(monitorsListArray));
 // End of creating a function
 module.exports = myMonitorsFunction;
+
