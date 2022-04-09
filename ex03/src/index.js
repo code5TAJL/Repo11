9b08d55179618bc0cabc0ba746940f494de7d0e2
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-09 21:26:23 +0200

    Added ex03/day12

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,7 +1,21 @@
 // Create a myPetsArray array here
-
+var myPetsArray = ["Dog", "Cat"];
 // End of myPetsArray array
 // Create a function below this line
+function myArrayFunction(myPets) {
+    var myNewPeets = [...myPets];
 
+    myNewPeets.push("Bird", "Fish");
+    var firstPet;
+    firstPet === myNewPeets[0];
+    var lastPet;
+    lastPet === myNewPeets[-1];
+    myNewPeets.pop();
+    myNewPeets.shift();
+    myNewPeets.unshift("Lion");
+
+    return myNewPeets;
+}
+console.log(myArrayFunction(myPetsArray));
 // End of creating a function
 module.exports = myArrayFunction;
