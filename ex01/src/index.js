7c6488e1370f8a5b4af34b2ac33aad28857e4008
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-09 21:02:55 +0200

    Added ex01/day11

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,20 @@
 // Create an array here
-
+var myArr = [2, 4, 0, 8, 10];
 // End of creating an array
 //Create a function below this line
+function myArrayFunction(myItems) {
+    var myItems = [...myArr];
+    myItems[2] = 6;
 
+    return myItems;
+    
+}
+console.log(myArrayFunction(myArr));  // Change this line
 // End of creating a function
+module.exports = myArrayFunction;
+
+
+
+
+
 module.exports = myArrayFunction;
